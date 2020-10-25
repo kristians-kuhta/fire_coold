2812d8d3daba74fbf87b7a76a35af86c00239c53
Kristians Kuhta <kristians.kuhta@gmail.com>
2020-10-25 21:53:39 +0200

    Add seeds for products
    * Add products.yml file that contains the products data with
      filenames for images
    * Add product images
    * Add product seeding within db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def seed_products
+  seed_filename = Rails.root.join('config/seeds/products.yml')
+  seed_file = File.read(seed_filename)
+
+  products = YAML.safe_load(seed_file).map(&:with_indifferent_access)
+
+  products.each do |attributes|
+    next if Product.where(name: attributes[:name]).exists?
+
+    image = File.open(attributes[:image], 'r')
+
+    product = Product.create!(attributes.except(:image))
+    # We will save all of files within seeds as png
+    product.image.attach(io: image, filename: "#{product.slug}.png")
+  end
+end
+
+ActiveRecord::Base.transaction do
+  seed_products
+end
